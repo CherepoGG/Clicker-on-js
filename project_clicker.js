934f5df3f957fb7aeba611e013ae2c95d75fc27c
Сизов Георгий Викторович <sizov.g@akvnzm.ru>
2020-02-13 21:44:20 +0500

    Доработаны модификации

--- project_clicker.js
+++ project_clicker.js
@@ -1,22 +1,29 @@
-﻿let progressCook = document.getElementById('progressCook');
+﻿//-------------------------------------------------глобальные данные
+
+let progressCook = document.getElementById('progressCook');
 let getCook = document.getElementById('getCook');
 let getMoney = document.getElementById('getMoney');
 let cookie = document.getElementById('cookie');
 let money = document.getElementById('money');
 let currentCookie = 0;
 let currentMoney = 0;
-let mods = {																//Цена и лвл каждого апдейта
-	Mixer: {
+let mods = {																                                      //Цена и лвл каждого апдейта
+	mixer: {
   	current_lvl: 0,
     base_cost: 1,
+    base_progress_click: 1,
   }, 
-	Spread: {
+	spread: {
   	current_lvl: 0,
     base_cost: 1,
+    base_price_cookie: 1,
   },
-  Cashbox: {
+  cashbox: {
   	current_lvl: 0,
     base_cost: 1,
+    base_cost_cookie: 0,
+    base_money: 1,
+    cashbox_started: false,
   },
   baker: {
 		current_lvl: 0,
@@ -26,8 +33,11 @@ let mods = {																//Цена и лвл каждого апдейта
   }
 }
 
-getCook.onclick = function() {							//Лепка печенья по нажатию
-  progressCook.value = progressCook.value + 10;
+//---------------------------------------------------- //кнопки продать/купить
+
+getCook.onclick = function() {							                                      //Лепка печенья по нажатию
+  let progress_click = mods.mixer.base_progress_click * mods.mixer.current_lvl;
+  progressCook.value = progressCook.value + 1 + progress_click;
   handlerClickСookie();
 }
 function handlerClickСookie(){
@@ -37,59 +47,121 @@ function handlerClickСookie(){
   }
 }
 
-getMoney.onclick = function() {							//продажа печенья по нажатию
-  money.innerHTML = currentMoney;
-  handlerClickSell();
-}
-function handlerClickSell(){
-	if(currentCookie >= 5) {								//Если печенек >= 5, то продаем 
-  	currentMoney = currentMoney + 5;		
-    money.innerHTML = currentMoney;
-    currentCookie = currentCookie - 5;		//кол-во печенья в общем на 1 монету
-    cookie.innerHTML = currentCookie;
+getMoney.onclick = function() {							                                      //продажа печенья по нажатию
+  if(currentCookie >= 1) {								                                        //Если печенек >= 5, то продаем 
+    let spread_value = mods.spread.base_price_cookie * mods.spread.current_lvl;
+    addMoney(1 + spread_value);
+    decreaseCookies(1);
   }
   else {
    alert("Недостаточно печенья!");
   }
 }
 
+//--------------------------------------------------//Ключевые функции
+
 function addCookies(count) {
 	currentCookie = currentCookie + count;
   cookie.innerHTML = currentCookie;
 }
+
 function decreaseCookies(count) {
-	currentCookie = currentCookie - count; 
-  cookie.innerHTML = currentCookie;
+  if(currentCookie - count >= 0) {
+    currentCookie = currentCookie - count;
+    cookie.innerHTML = currentCookie;
+    return true;
+    }
+  return false;
 }
+
 function addMoney(count) {
-	currentMoney = currentMoney + count;
+  currentMoney = currentMoney + count;
   money.innerHTML = currentMoney;
 }
+
 function decreaseMoney(count) {
-	currentMoney = currentMoney - count;
-  money.innerHTML = currentMoney;
+  if(currentMoney - count >= 0) {
+	  currentMoney = currentMoney - count;
+    money.innerHTML = currentMoney;
+    return true;
+  }
+  return false;
 }
+
+//-------------------------------------------------------//Контроль выбора апгрейда
 
 let upgrades = document.getElementsByName('upgrade');
 document.getElementById('upgrader').onclick = checkRadio;
 
-function checkRadio() {
+function checkRadio() {                                                           //Проверка выбранного пункта
   let allUpgrades = document.getElementsByName('upgrade');
   for(i = 0; i < allUpgrades.length; i++) {
     if(allUpgrades[i].checked) {
 			val_input = allUpgrades[i].value;
       
     switch(val_input) {
-      case "mixer": alert('mixer');
+      case "mixer": upgrade_mixer();
       break;
-      case "spread": alert('spread');
+      case "spread": upgrade_spread();
       break;
-      case "cashbox": alert('cashbox');
+      case "cashbox": upgrade_cashbox();
       break;
       case "baker": upgrade_baker();
      	break;
       }
     }
+  }
+}
+
+//-------------------------------------------------------------Работа апгрейдов
+
+function upgrade_mixer() {
+  let up_lvl = mods.mixer.current_lvl + 1;
+  let up_cost = mods.mixer.base_cost * up_lvl;
+  if( decreaseMoney(up_cost) ) {
+    mods.mixer.current_lvl = up_lvl;
+  }
+  else {
+    alert("Недостаточно денег!");
+  }
+}
+
+function upgrade_spread() {
+  let up_lvl = mods.spread.current_lvl + 1;
+  let up_cost = mods.spread.base_cost * up_lvl;
+  if( decreaseMoney(up_cost) ) {
+    mods.spread.current_lvl = up_lvl;
+  }
+  else {
+    alert("Недостаточно денег!");
+  }
+}
+
+function work_spread() {
+  let money = mods.spread.base_price_cookie * mods.spread.current_lvl;
+  addMoney(money); 
+}
+
+function upgrade_cashbox() {
+  let up_lvl = mods.cashbox.current_lvl + 1;
+  let up_cost = mods.cashbox.base_cost * up_lvl;
+  let up_cost_cookie = mods.cashbox.base_cost_cookie + 1;
+  if(currentMoney >= up_cost & currentCookie >= up_cost_cookie) {
+    mods.cashbox.current_lvl = up_lvl;
+    mods.cashbox.base_cost_cookie = up_cost_cookie;
+    decreaseMoney(up_cost);
+  }
+  else {
+    alert("Недостаточно денег или печенья!");
+  }
+}
+
+function work_cashbox() {
+  let money = mods.cashbox.base_money * mods.cashbox.current_lvl;
+  let cookie = mods.cashbox.base_cost_cookie
+  if(currentCookie >= cookie) {
+    addMoney(money);
+    decreaseCookies(cookie);
   }
 }
 
@@ -99,17 +171,19 @@ function upgrade_baker() {
   if(currentMoney >= up_cost) {
     mods.baker.current_lvl = up_lvl;
     decreaseMoney(up_cost);
-    if(mods.baker.baker_started === false) {
-    	setInterval(add_cookie_baker, 1000);
-      mods.baker.baker_started = true;
-    }
   }
   else {
     alert("Недостаточно денег!");
   }
 }
 
-function add_cookie_baker() {
+function work_baker() {
   let cookies = mods.baker.base_cookie * mods.baker.current_lvl;
   addCookies(cookies);
 }
+
+function startMods() {
+  work_cashbox();
+  work_baker();
+}
+setInterval(startMods, 1000);
