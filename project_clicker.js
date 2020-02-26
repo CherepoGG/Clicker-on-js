182445183c1e39cf587a6a06c1cf475853968498
CherepoGG <Sizov98@inbox.ru>
2020-02-26 10:17:07 +0500

    Test upgrade cashbox & baker

--- project_clicker.js
+++ project_clicker.js
@@ -1,4 +1,4 @@
-﻿//-------------------------------------------------глобальные данные
+﻿//---------------------------------------------------------------------глобальные данные
 
 let progressCook = document.getElementById('progressCook');
 let getCook = document.getElementById('getCook');
@@ -13,6 +13,8 @@ let counter_mixer = document.getElementById("counter_mixer");
 let counter_spread = document.getElementById("counter_spread");
 let counter_cashbox = document.getElementById("counter_cashbox");
 let counter_baker = document.getElementById("counter_baker");
+let progress_auto_sell = document.getElementById("progress_auto_sell");
+let progress_auto_cook = document.getElementById("progress_auto_cook");
 let currentCookie = 0;
 let currentMoney = 0;
 
@@ -41,9 +43,11 @@ let mods = {																                                            //Цен
     baker_started: false,
   }
 }
-//----------------------------------------------------- проверка денег и активация/деактивация апгрейдов
+//------------------------------ проверка денег и активация/деактивация кнопок апгрейдов
 function check_money_for_mixer() {
-  if(currentMoney >= mods.mixer.up_cost) {
+  let up_lvl = mods.mixer.current_lvl + 1;
+  let up_cost = mods.mixer.base_cost * up_lvl;
+  if(currentMoney >= up_cost) {
     button_upgrade_mixer.disabled = false;
   }
   else {
@@ -52,16 +56,20 @@ function check_money_for_mixer() {
 }
 
 function check_money_for_spread() {
-  if(currentMoney >= mods.spread.base_cost) {
+  let up_lvl = mods.spread.current_lvl + 1;
+  let up_cost = mods.spread.base_cost * up_lvl;
+  if(currentMoney >= up_cost) {
     button_upgrade_spread.disabled = false;
   }
   else {
-    button_upgrade_spread.disabled = true;
+    button_upgrade_spread.disabled = true; 
   }
 }
 
 function check_money_for_cashbox() {
-  if(currentMoney >= mods.cashbox.base_cost) {
+  let up_lvl = mods.cashbox.current_lvl + 1;
+  let up_cost = mods.cashbox.base_cost * up_lvl;
+  if(currentMoney >= up_cost) {
     button_upgrade_cashbox.disabled = false;
   }
   else {
@@ -70,7 +78,9 @@ function check_money_for_cashbox() {
 }
 
 function check_money_for_baker() {
-  if(currentMoney >= mods.baker.base_cost) {
+  let up_lvl = mods.baker.current_lvl + 1;
+  let up_cost = mods.baker.base_cost * up_lvl;
+  if(currentMoney >= up_cost) {
     button_upgrade_baker.disabled = false;
   }
   else {
@@ -79,38 +89,45 @@ function check_money_for_baker() {
 }
 
 
-//---------------------------------------------------- кнопки продать/купить
-
-getCook.onmousedown = function () {
-  let element = document.getElementById("getCook");
-  element.classList.toggle("input_cookie_click");
+//-----------------------------------------------------------------кнопки продать/купить
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-getCook.onmouseup = function () {
+getCook.ondragstart = function() {
+  return false;
+};
+
+async function animation_cookie() {
   let element = document.getElementById("getCook");
-  element.classList.toggle("input_cookie_click");
+  element.classList.toggle("animation_cook_click");
+  await sleep(65);
+  element.classList.toggle("animation_cook_click");
 }
 
 getCook.onclick = function() {							                                            //Лепка печенья по нажатию
+  animation_cookie();
   let progress_click = mods.mixer.base_progress_click * mods.mixer.current_lvl;
   progressCook.value = progressCook.value + 1 + progress_click;
-  if(progressCook.value === 10) {
+  if(progressCook.value === progressCook.max) {
   	addCookies(1);
     progressCook.value = 0;
   }
 }
 
-getMoney.onmousedown = function () {
+getMoney.ondragstart = function() {
+  return false;
+};
+
+async function animation_money() {
   let element = document.getElementById("getMoney");
-  element.classList.toggle("input_sell_click");
+  element.classList.toggle("animation_sell_click");
+  await sleep(65);
+  element.classList.toggle("animation_sell_click");
 }
 
-getMoney.onmouseup = function () {
-  let element = document.getElementById("getMoney");
-  element.classList.toggle("input_sell_click");
-}
-
-getMoney.onclick = function() {							                                            //продажа печенья по нажатию
+getMoney.onclick = function() {                                                         //продажа печенья по нажатию
+  animation_money();
   if(currentCookie >= 1) {								                                              //Если печенек >= 5, то продаем 
     let spread_value = mods.spread.base_price_cookie * mods.spread.current_lvl;
     addMoney(1 + spread_value);
@@ -121,7 +138,7 @@ getMoney.onclick = function() {							                                          
   }
 }
 
-//--------------------------------------------------Ключевые функции
+//----------------------------------------------------------------------Ключевые функции
 
 function addCookies(count) {
 	currentCookie = currentCookie + count;
@@ -151,7 +168,7 @@ function decreaseMoney(count) {
   return false;
 }
 
-//-----------------------------------------------------Контроль выбора апгрейда
+//--------------------------------------------------------------Контроль выбора апгрейда
 
 button_upgrade_mixer.onclick = function() {
   upgrade_mixer();
@@ -168,13 +185,13 @@ button_upgrade_cashbox.onclick = function() {
 button_upgrade_baker.onclick = function() {
   upgrade_baker();
 }
-//-------------------------------------------------------------Работа апгрейдов
+//----------------------------------------------------------------------Работа апгрейдов
 
 function upgrade_mixer() {
   let up_lvl = mods.mixer.current_lvl + 1;
   let up_cost = mods.mixer.base_cost * up_lvl;
-  if( decreaseMoney(mods.mixer.up_cost) ) {
-    mods.mixer.current_lvl = mods.mixer.up_lvl;
+  if( decreaseMoney(up_cost) ) {
+    mods.mixer.current_lvl = up_lvl;
   }
   else {
     alert("Недостаточно денег!");
@@ -201,22 +218,56 @@ function upgrade_cashbox() {
   let up_lvl = mods.cashbox.current_lvl + 1;
   let up_cost = mods.cashbox.base_cost * up_lvl;
   let up_cost_cookie = mods.cashbox.base_cost_cookie + 1;
-  if(currentMoney >= up_cost & currentCookie >= up_cost_cookie) {
+  if(currentMoney >= up_cost && currentCookie >= up_cost_cookie) {
     mods.cashbox.current_lvl = up_lvl;
     mods.cashbox.base_cost_cookie = up_cost_cookie;
     decreaseMoney(up_cost);
   }
   else {
-    alert("Недостаточно денег или печенья!");
+    alert("Недостаточно печенья!");
+  }
+}
+
+function start_cashbox_pb() {
+  if(mods.cashbox.current_lvl >= 1) {
+    progress_auto_sell.value += 1;
+  }
+}
+
+function upgrade_cashbox_speed() {
+  switch(mods.cashbox.current_lvl) {
+    case 1: progress_auto_sell.max = 10;
+      break;
+    case 2: progress_auto_sell.max = 9;
+      break;
+    case 3: progress_auto_sell.max = 8;
+      break;
+    case 4: progress_auto_sell.max = 7;
+      break;
+    case 5: progress_auto_sell.max = 6;
+      break;
+    case 6: progress_auto_sell.max = 5;
+      break;
+    case 7: progress_auto_sell.max = 4;
+      break;
+    case 8: progress_auto_sell.max = 3;
+      break;
+    case 9: progress_auto_sell.max = 2;
+      break;
+    case 10: progress_auto_sell.max = 1;
+      break;
   }
 }
 
 function work_cashbox() {
   let money = mods.cashbox.base_money * mods.cashbox.current_lvl;
   let cookie = mods.cashbox.base_cost_cookie;
-  if(currentCookie >= cookie) {
+  if(mods.cashbox.current_lvl >= 1 && currentCookie >= cookie && progress_auto_sell.value === progress_auto_sell.max) {
+    progress_auto_sell.value = 0;
+    progress_auto_sell.innerHTML;
     addMoney(money);
     decreaseCookies(cookie);
+    work_spread();
   }
 }
 
@@ -232,14 +283,51 @@ function upgrade_baker() {
   }
 }
 
+function start_baker_pb() {
+  if(mods.baker.current_lvl >= 1) {
+    progress_auto_cook.value += 1;
+  }
+}
+
+function upgrade_baker_speed() {
+  switch(mods.baker.current_lvl) {
+    case 1: progress_auto_cook.max = 10;
+      break;
+    case 2: progress_auto_cook.max = 9;
+      break;
+    case 3: progress_auto_cook.max = 8;
+      break;
+    case 4: progress_auto_cook.max = 7;
+      break;
+    case 5: progress_auto_cook.max = 6;
+      break;
+    case 6: progress_auto_cook.max = 5;
+      break;
+    case 7: progress_auto_cook.max = 4;
+      break;
+    case 8: progress_auto_cook.max = 3;
+      break;
+    case 9: progress_auto_cook.max = 2;
+      break;
+    case 10: progress_auto_cook.max = 1;
+      break;
+  }
+}
+
 function work_baker() {
   let cookies = mods.baker.base_cookie * mods.baker.current_lvl;
-  addCookies(cookies);
+  if(progress_auto_cook.value === progress_auto_cook.max) {
+    addCookies(cookies);
+    progress_auto_cook.value = 0;
+    progress_auto_cook.innerHTML;
+  }
 }
 
 function startMods() {
   work_cashbox();
+  start_cashbox_pb();
   work_baker();
+  start_baker_pb();
 }
 setInterval(startMods, 1000);
 
@@ -254,5 +342,7 @@ function start_status() {
   counter_spread.innerHTML = mods.spread.current_lvl;
   counter_cashbox.innerHTML = mods.cashbox.current_lvl;
   counter_baker.innerHTML = mods.baker.current_lvl;
+  upgrade_baker_speed();
+  upgrade_cashbox_speed();
 }
-setInterval (start_status, 1);
+setInterval(start_status, 1);
